2a254e062e76c9a95c72a5b4f7a88ce8b2013fb3
hnhtran <tran_eetas@yahoo.com>
2022-06-15 03:30:46 +1000

    linked bankNew to bankIndex

--- controllers/bank-acc-routers.js
+++ controllers/bank-acc-routers.js
@@ -3,8 +3,14 @@ const express = require('express')
 const bankAcc = require('../models/bankacc')
 const router = express.Router()
 
+// index route
 router.get('/', (req, res) => {
     res.render('bankacc/index')
+})
+
+// new transaction route
+router.get('/new', (req, res) => {
+    res.render('bankacc/new')
 })
 
 module.exports = router
