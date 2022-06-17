98f157d096bd6d2471501b1bea3b83afe86a609b
hnhtran <tran_eetas@yahoo.com>
2022-06-18 01:45:41 +1000

    heroku deployment

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
 //=========================================
 // Server listener
 //=========================================
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
     routesReport.print();
